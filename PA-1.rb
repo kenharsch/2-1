bf600ba0a391f50392a6ef63929936cce5b31716
Ken Harsch <kharsch@brandeis.edu>
2014-01-17 11:28:03 -0500

    Third commit

--- PA-1.rb
+++ PA-1.rb
@@ -1,2 +1,9 @@
-puts "Hello World!"
-
+class Movie
+  def self.load_data
+    lines = []
+    File.new("c:\\Users\\Ken\\Desktop\\236 S Eng\\u.data")
+    lines << line
+    store lines
+    puts lines[2]
+  end
+end
